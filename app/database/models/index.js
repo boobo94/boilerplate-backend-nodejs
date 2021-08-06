f2d0d2fd8cfb98061f1d8048b1ab4ba8b46aad0f
boobo94 <militaru.bogdanalexandru@gmail.com>
2021-08-06 16:09:50 +0300

    Fix import in models declaration

--- app/database/models/index.js
+++ app/database/models/index.js
@@ -23,7 +23,7 @@ fs
   .readdirSync(__dirname)
   .filter((file) => (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js'))
   .forEach((file) => {
-    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
+    const model = require(path.join(__dirname, file)).default(sequelize, Sequelize.DataTypes);
     db[model.name] = model;
   });
 
