4d981e684ba8078ceac13ac66432c6b928ad4eae
boobo94 <militaru.bogdanalexandru@gmail.com>
2021-08-06 08:59:15 +0300

    Change esm with babel

--- app/index.js
+++ app/index.js
@@ -1,3 +1,24 @@
-// Set options as a parameter, environment variable, or rc file.
-require = require("esm")(module/*, options*/)
-module.exports = require("./main.js")
+/* eslint-disable import/first */
+require('dotenv').config();
+
+import express from 'express';
+import cors from 'cors';
+// import morgan from 'morgan';
+import router from './routers';
+import { InitDB } from './database';
+
+const app = express();
+
+app.use(cors());
+// app.use(morgan('common'));
+
+app.use(express.json({ limit: '2mb' }));
+
+// initialize router
+app.use('/', router);
+
+// initialize database
+InitDB();
+
+// eslint-disable-next-line no-console
+app.listen(process.env.PORT, () => console.log(`Server is up and running on port ${process.env.PORT}`));
