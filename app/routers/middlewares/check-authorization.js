26f230b9273eaefb6f2fffc665e057fec934c240
boobo94 <militaru.bogdanalexandru@gmail.com>
2021-08-07 15:27:20 +0300

    Setup authorization middleware #6

---
+++ app/routers/middlewares/check-authorization.js
@@ -0,0 +1,27 @@
+import statusCodes from '../../services/status-codes';
+import errors from '../../locales/errors.json';
+import { DecodeJWT } from '../../services/jwt';
+import { findOne } from '../../database/services/user';
+
+export default async (req, res, next) => {
+  try {
+    // check if user exists
+    const { userId } = DecodeJWT(req.headers.authorization);
+    const user = await findOne(userId);
+    if (!user) {
+      return res.status(statusCodes.UNAUTHORIZED).send({ error: errors.UNAUTHORIZED });
+    }
+
+    req.userId = userId;
+    req.companyId = user.company_id;
+  } catch (err) {
+    console.log('err', err);
+    if (err.name === 'TokenExpiredError') {
+      return res.status(statusCodes.UNAUTHORIZED).send({ error: errors.JWT_EXPIRED });
+    }
+
+    return res.status(statusCodes.UNAUTHORIZED).send({ error: errors.UNAUTHORIZED });
+  }
+
+  return next();
+};
