ed33f887fc7a462a622eb552a0000121eba5fb76
boobo94 <militaru.bogdanalexandru@gmail.com>
2022-02-07 16:23:56 +0200

    Disable different path for root app

--- client-app/vue.config.js
+++ client-app/vue.config.js
@@ -1,5 +1,4 @@
  module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-  ? '/app/'
-  : '/'
+  // change this if you'd like to run on a different path than root
+  // publicPath: process.env.NODE_ENV === 'production' ? '/app/' : '/'
 }
